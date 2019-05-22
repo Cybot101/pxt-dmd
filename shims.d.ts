ffe7ba7e083cdd1e378f76ac3cca823cdcaad251
Kyle Harris <cybot101@gmail.com>
2019-05-22 22:48:51 +1000

    test

---
+++ shims.d.ts
@@ -0,0 +1,17 @@
+// Auto-generated. Do not edit.
+
+
+    /**
+     * DMD operations
+     */
+    //% weight=5 color=#002050 icon="\uf0a0"
+declare namespace dmd {
+
+    /**
+     */
+    //% blockId="update_display" block="update display"
+    //% blockExternalInputs=1 weight=90 blockGap=8 shim=dmd::updateDisplay
+    function updateDisplay(): void;
+}
+
+// Auto-generated. Do not edit. Really.
