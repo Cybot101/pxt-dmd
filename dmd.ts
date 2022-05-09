eec6e91cc48540fbec05137225c893023ede0850
Kyle Harris <kyle.harris@edenworth.com>
2022-05-09 22:43:05 +1000

    Working version

--- dmd.ts
+++ dmd.ts
@@ -1,82 +1,171 @@
 
 /**
- * Use this file to define custom functions and blocks.
- * Read more at https://makecode.microbit.org/blocks/custom
+ * LED Dot Matrix Display interface
  */
 
-const i2cAddr = 8;
+ const i2cAddr = 126;
 
-enum G_COMMAND {
-    G_NONE = 0,
-    G_CLEAR,
-    G_POINT,
-    G_LINE,
-    G_CIRCLE,
-    G_RECTANGLE,
-    G_TEXT,
-    G_SCROLL
-}
-
-/**
- * Custom blocks
- */
-//% weight=100 color=#0fbc11 icon=""
-namespace dmd {
-    
-    /**
-     * Clear a single point on display
-     * @param x X location
-     * @param y Y location
-     */
-    //% block
-    export function clearPoint(x: number, y: number): void {
-        let buf: Buffer = pins.createBuffer(5);
-        buf[0] = 0x10;
-        buf[1] = G_COMMAND.G_POINT;
-        buf[2] = x;
-        buf[3] = y;
-        buf[4] = 0;
-        pins.i2cWriteBuffer(i2cAddr, buf);
-    }
-    /**
-     * Draw a single point on display
-     * @param x X location
-     * @param y Y location
-     */
-    //% block
-    export function drawPoint(x: number, y: number): void {
-        let buf: Buffer = pins.createBuffer(5);
-        buf[0] = 0x10;
-        buf[1] = G_COMMAND.G_POINT;
-        buf[2] = x;
-        buf[3] = y;
-        buf[4] = 1;
-        pins.i2cWriteBuffer(i2cAddr, buf);
-    }
-
-    /**
-     * Draw a circle on display
-     * @param x X location
-     * @param y Y location
-     * @param r Radius
-     */
-    //% block
-    export function drawCircle(x: number, y: number, r: number): void {
-        let buf: Buffer = pins.createBuffer(5);
-        buf[0] = 0x10;
-        buf[1] = G_COMMAND.G_CIRCLE;
-        buf[2] = x;
-        buf[3] = y;
-        buf[4] = r;
-        pins.i2cWriteBuffer(i2cAddr, buf);
-    }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
-    }
-}
+ enum G_COMMAND {
+     G_NONE = 0,
+     G_CLEAR,
+     G_POINT,
+     G_LINE,
+     G_CIRCLE,
+     G_RECTANGLE,
+     G_TEXT,
+     G_SCROLL
+ }
+ 
+ enum G_COLOUR {
+     G_BLACK,
+     G_RED
+ }
+ 
+ /**
+  * Custom blocks
+  */
+ //% weight=100 color=#0fbc11 icon="\uf0ce" block="DMD"
+ namespace dmd {
+ 
+     /**
+      * Fill the whole screen
+      * @param colour Colour (0 = off, 1 = on)
+      */
+     //% block="fill screen with|colour %colour"
+     //% colour.min=0 colour.max=1
+     //% colour.fieldOptions.precision=1
+     export function fillScreen(colour: number): void {
+         let buf: Buffer = pins.createBuffer(10);
+         buf[0] = G_COMMAND.G_CLEAR;
+         buf[1] = colour;
+         pins.i2cWriteBuffer(i2cAddr, buf);
+     }
+ 
+     /**
+      * Draw a single point on display
+      * @param x X location
+      * @param y Y location
+      * @param colour Colour (0 = off, 1 = on)
+      */
+     //% block="draw point|x %x|y %y|colour %colour"
+     //% x.min=0 x.max=31 y.min=0 y.max=15
+     //% x.fieldOptions.precision=1 y.fieldOptions.precision=1
+     //% colour.fieldOptions.precision=1
+     //% colour.min=0 colour.max=1
+     //% colour.fieldOptions.precision=1
+     export function drawPoint(x: number, y: number, colour: number): void {
+         let buf: Buffer = pins.createBuffer(10);
+         buf[0] = G_COMMAND.G_POINT;
+         buf[1] = x;
+         buf[2] = y;
+         buf[3] = colour;
+         pins.i2cWriteBuffer(i2cAddr, buf);
+     }
+ 
+     /**
+      * Draw a circle on display
+      * @param x X location
+      * @param y Y location
+      * @param r Radius
+      * @param colour Colour (0 = off, 1 = on)
+      */
+     //% block="draw circle|x %x|y %y|radius %r|fill? %fill|colour %colour"
+     //% x.min=0 x.max=31 y.min=0 y.max=15 r.min=1 r.max=31
+     //% x.fieldOptions.precision=1 y.fieldOptions.precision=1 r.fieldOptions.precision=1
+     //% colour.fieldOptions.precision=1
+     //% colour.min=0 colour.max=1
+     //% colour.fieldOptions.precision=1
+     export function drawCircle(x: number, y: number, r: number, fill: boolean, colour: number): void {
+         let buf: Buffer = pins.createBuffer(10);
+         buf[0] = G_COMMAND.G_CIRCLE;
+         buf[1] = x;
+         buf[2] = y;
+         buf[3] = r;
+         buf[4] = fill ? 1 : 0;
+         buf[5] = colour;
+         pins.i2cWriteBuffer(i2cAddr, buf);
+     }
+ 
+     /**
+      * Draw a rectangle on display
+      * @param x1 First X location
+      * @param y1 First Y location
+      * @param x2 Second X location
+      * @param y3 Second Y location
+      * @param fill Filled rectangle?
+      * @param colour Colour (0 = off, 1 = on)
+      */
+     //% block="draw rectangle|start x %x1|start y %y1|end x %x2|end y %y2|fill? %fill|colour %colour"
+     //% x1.min=0 x1.max=31 y1.min=0 y1.max=15
+     //% x2.min=0 x2.max=31 y2.min=0 y2.max=15
+     //% colour.min=0 colour.max=1
+     //% colour.fieldOptions.precision=1
+     //% x1.fieldOptions.precision=1 y1.fieldOptions.precision=1
+     //% x2.fieldOptions.precision=1 y2.fieldOptions.precision=1
+     //% colour.fieldOptions.precision=1
+     export function drawRectangle(x1: number, y1: number, x2: number, y2: number, fill: boolean, colour: number): void {
+         let buf: Buffer = pins.createBuffer(10);
+         buf[0] = G_COMMAND.G_RECTANGLE;
+         buf[1] = x1;
+         buf[2] = y1;
+         buf[3] = x2;
+         buf[4] = y2;
+         buf[5] = fill ? 1 : 0;
+         buf[6] = colour;
+         pins.i2cWriteBuffer(i2cAddr, buf);
+     }
+ 
+     /**
+      * Draw a line on display
+      * @param x1 First X location
+      * @param y1 First Y location
+      * @param x2 Second X location
+      * @param y3 Second Y location
+      * @param colour Colour (0 = off, 1 = on)
+      */
+     //% block="draw line|start x %x1|start y %y1|end x %x2|end y %y2|colour %colour"
+     //% x1.min=0 x1.max=31 y1.min=0 y1.max=15
+     //% x2.min=0 x2.max=31 y2.min=0 y2.max=15
+     //% x1.fieldOptions.precision=1 y1.fieldOptions.precision=1
+     //% x2.fieldOptions.precision=1 y2.fieldOptions.precision=1
+     //% colour.min=0 colour.max=1
+     //% colour.fieldOptions.precision=1
+     export function drawLine(x1: number, y1: number, x2: number, y2: number, colour: number): void {
+         let buf: Buffer = pins.createBuffer(10);
+         buf[0] = G_COMMAND.G_LINE;
+         buf[1] = x1;
+         buf[2] = y1;
+         buf[3] = x2;
+         buf[4] = y2;
+         buf[5] = colour;
+         pins.i2cWriteBuffer(i2cAddr, buf);
+     }
+ 
+     /**
+      * Draw a short string of text
+      * @param x X location
+      * @param y Y location
+      * @param text Text to display
+      * @param colour Colour (0 = off, 1 = on)
+      */
+     //% block="draw text|x %x|y %y|text %text|colour %colour"
+     //% x.min=0 x.max=31 y.min=0 y.max=15 text.maxLength=6
+     //% x.fieldOptions.precision=1 y.fieldOptions.precision=1
+     //% colour.min=0 colour.max=1
+     //% colour.fieldOptions.precision=1
+     export function drawText(x: number, y: number, text: string, colour: number): void {
+         let buf: Buffer = pins.createBuffer(10);
+         buf[0] = G_COMMAND.G_TEXT;
+         buf[1] = x;
+         buf[2] = y;
+         buf[3] = colour;
+         buf[4] = text.charCodeAt(0);
+         buf[5] = text.charCodeAt(1);
+         buf[6] = text.charCodeAt(2);
+         buf[7] = text.charCodeAt(3);
+         buf[8] = text.charCodeAt(4);
+         buf[9] = text.charCodeAt(5);
+         pins.i2cWriteBuffer(i2cAddr, buf);
+     }
+ }
+ 
--- shims.d.ts
+++ shims.d.ts
@@ -7,11 +7,6 @@
     //% weight=5 color=#002050 icon="\uf0a0"
 declare namespace dmd {
 
-    /**
-     */
-    //% blockId="update_display" block="update display"
-    //% blockExternalInputs=1 weight=90 blockGap=8 shim=dmd::updateDisplay
-    function updateDisplay(): void;
 }
 
 // Auto-generated. Do not edit. Really.
